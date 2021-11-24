fce7f49cdc5fa14ebac20a267b8603f7533d93f4
Jocimar B. A. Huss <jocimarspi@gmail.com>
2021-11-23 21:52:39 -0300

    Home content. First part

---
+++ src/pages/Home/index.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./style.scss";
+import emphasisImage from "../../images/emphasis-image.png";
+import Pills from "../../components/Pills";
+
+const PLACES = ["Praça", "Parque", "Igreja", "Hotel", "Farmácia", "Loja"];
+
+const Home = () => {
+  const [selectedPill, setSelectedPill] = useState("");
+
+  return (
+    <main id="main-content" className="home__container">
+      <div className="home__col">
+        <h1 className="home__title">Mandaguari para Todos</h1>
+        <p className="home__text">
+          Nossa missão é facilitar o encontro entre lugares inclusivos e pessoas
+          que buscam alternativas culturais para sair de casa, com a mobilidade
+          que todo cidadão precisa.
+        </p>
+        <p className="home__text">
+          Checamos 10 itens essenciais de acessibilidade para pessoas com
+          deficiência em diversos pontos da cidade.
+        </p>
+        <div className="home__pills">
+          {PLACES.map((item) => {
+            return (
+              <Pills
+                local={item}
+                selected={selectedPill === item}
+                onClick={() => setSelectedPill(item)}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div className="home__col">
+        <div className="home__image home__image--emphasis">
+          <img src={emphasisImage} alt="Imagem cadeirante" />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
