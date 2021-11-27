eea2cbe5c35018f01fa13af172e87bc5b11d48b1
Jocimar B. A. Huss <jocimarspi@gmail.com>
2021-11-27 18:35:18 -0300

    Dinamic content with json-server

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,56 +2,74 @@ import React from "react";
 import Button from "../Button";
 import "./card.scss";
 
-import iconAudio from '../../images/icon-audio-descricao.svg';
-import iconBanheiro from '../../images/icon-banheiro-acessivel.png';
-import iconBraile from '../../images/icon-braile.png';
-import iconCaoGuia from '../../images/icon-caoguia.png';
-import iconElevador from '../../images/icon-elevador.png';
-import iconEstacionamento from '../../images/icon-estacionamento.png';
-import iconLibras from '../../images/icon-libras.png';
-import iconPisoTatil from '../../images/icon-piso-tatil.png';
-import iconPortaLarga from '../../images/icon-porta-larga.png';
-import iconRampa from '../../images/icon-rampa.png';
+import iconAudio from "../../images/icon-audio-descricao.svg";
+import iconBanheiro from "../../images/icon-banheiro-acessivel.png";
+import iconBraile from "../../images/icon-braile.png";
+import iconCaoGuia from "../../images/icon-caoguia.png";
+import iconElevador from "../../images/icon-elevador.png";
+import iconEstacionamento from "../../images/icon-estacionamento.png";
+import iconLibras from "../../images/icon-libras.png";
+import iconPisoTatil from "../../images/icon-piso-tatil.png";
+import iconPortaLarga from "../../images/icon-porta-larga.png";
+import iconRampa from "../../images/icon-rampa.png";
 
-const Card = () => {
+const facilities = [
+  { id: 1, name: "Sinalização em Braile", icon: iconBraile },
+  { id: 2, name: "Audio descrição", icon: iconAudio },
+  { id: 3, name: "Banheiro acessível", icon: iconBanheiro },
+  { id: 4, name: "Elevador para cadeirantes", icon: iconElevador },
+  {
+    id: 5,
+    name: "Estacionamento para idosos / deficientes",
+    icon: iconEstacionamento,
+  },
+  { id: 6, name: "Permite acesso de cães-guias", icon: iconCaoGuia },
+  { id: 7, name: "Piso tátil de alerta", icon: iconPisoTatil },
+  { id: 8, name: "Portas largas", icon: iconPortaLarga },
+  { id: 9, name: "Rampas de acesso para cadeirante", icon: iconRampa },
+  { id: 10, name: "Atendimento em Libras", icon: iconLibras },
+];
+
+const isChecked = (facility, place) => {
+  const findedFacility = place.facilities.find((currentFacility) => {
+    return currentFacility.item === facility.id;
+  });
+
+  if (!findedFacility) {
+    return false;
+  } else {
+    return findedFacility.status;
+  }
+};
+
+const Card = ({ place }) => {
+  const imagePath = require('../../images' + place.image);
   return (
     <div className="    ">
-      <div className="card__image"></div>
+      <div className="card__image">
+        <img src={imagePath.default} alt={place.name} title={place.name} />
+      </div>
       <div className="card__content">
-        <h3>Nome do local</h3>
+        <h3>{place.name}</h3>
         <ul className="card__facilities">
-          <li className="card__facilities__item card__facilities__item--unchecked">
-              <img src={iconBraile} alt="Sinalização em Braile" title="Sinalização em Braile" />
-          </li>
-          <li className="card__facilities__item card__facilities__item">
-              <img src={iconAudio} alt="Audio descrição" title="Audio descrição" />
-          </li>
-          <li className="card__facilities__item">
-              <img src={iconBanheiro} alt="Banheiro acessível" title="Banheiro acessível" />
-          </li>
-          <li className="card__facilities__item">
-              <img src={iconElevador} alt="Elevador para cadeirantes" title="Elevador para cadeirantes" />
-          </li>
-          <li className="card__facilities__item">
-              <img src={iconEstacionamento} alt="Estacionamento para idosos / deficientes" title="Estacionamento para idosos / deficientes" />
-          </li>
-          <li className="card__facilities__item card__facilities__item--unchecked">
-              <img src={iconCaoGuia} alt="Permite acesso de cães-guias" title="Permite acesso de cães-guias" />
-          </li>
-          <li className="card__facilities__item">
-              <img src={iconPisoTatil} alt="Piso tátil de alerta" title="Piso tátil de alerta" />
-          </li>
-          <li className="card__facilities__item">
-              <img src={iconPortaLarga} alt="Portas largas" title="Portas largas" />
-          </li>
-          <li className="card__facilities__item card__facilities__item--unchecked">
-              <img src={iconRampa} alt="Rampas de acesso para cadeirante" title="Rampas de acesso para cadeirante" />
-          </li>
-          <li className="card__facilities__item">
-              <img src={iconLibras} alt="Atendimento em Libras" title="Atendimento em Libras" />
-          </li>          
+          {facilities.map((facility) => {
+            return (
+              <li
+                key={facility.id}
+                className={`card__facilities__item card__facilities__item${
+                  isChecked(facility, place) ? "" : "--unchecked"
+                }`}
+              >
+                <img
+                  src={facility.icon}
+                  alt={facility.name}
+                  title={facility.name}
+                />
+              </li>
+            );
+          })}
         </ul>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <p>{place.description}</p>
 
         <Button>Como chegar?</Button>
       </div>
--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,46 +1,52 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import SwiperCore, { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Card from "../Card";
 import "./slider.scss";
 import "swiper/swiper-bundle.css";
+import api from "../../config/api";
+import { FilterContext } from "../../contexts/FilterContext";
 
 SwiperCore.use([Pagination]);
 
 const Slider = () => {
+  const { filteredPlaces, setFilteredPlaces} = useContext(FilterContext);
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      const result = await api.get(`/places?category=${filteredPlaces}`);
+
+      if (result.status === 200) {
+        setPlaces(result.data);
+      }
+    };
+
+    fetchPlaces();
+  }, [filteredPlaces]);
+
   return (
     <Swiper
       slidesPerView={1}
       breakpoints={{
+        300: {
+          slidesPerView: 1
+        },
         767: {
-          slidesPerView: 2,
+          slidesPerView: 2
         },
         1024: {
-          slidesPerView: 4,
+          slidesPerView: 4
         },
       }}
     >
-      <SwiperSlide>
-        <Card />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Card />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Card />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Card />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Card />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Card />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Card />
-      </SwiperSlide>
+      {places.map((place) => {
+        return (
+          <SwiperSlide>
+            <Card key={place.id} place={place} />
+          </SwiperSlide>
+        );
+      })}      
     </Swiper>
   );
 };
